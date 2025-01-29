b5bbdf95b8ea79f0543dee2e5c51444b2c7b752f
zfosix <fajarfauzian53@gmail.com>
2025-01-29 12:13:58 +0700

    updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,26 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: ["class"],
+  darkMode: ["class"], // Dark mode berdasarkan kelas
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)'
+        background: 'var(--background)', // Pastikan variabel ini ada di file CSS
+        foreground: 'var(--foreground)', // Pastikan variabel ini ada di file CSS
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
+        lg: 'var(--radius)', // Pastikan variabel ini ada di file CSS
+        md: 'calc(var(--radius) - 2px)', 
+        sm: 'calc(var(--radius) - 4px)', 
       }
     }
   },
-} satisfies Config;
+  plugins: [],
+};
+
+export default config;
