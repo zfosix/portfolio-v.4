bb5c85561ccebc078ab88aa225ed61b40ed30ed5
zfosix <fajarfauzian53@gmail.com>
2025-02-27 22:11:23 +0700

    updated

--- src/components/sidebar/MobileMenu.tsx
+++ src/components/sidebar/MobileMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Profile from '@/components/sidebar/Profile';
+import Profile from "@/components/sidebar/Profile";
 import FooterItem from "@/components/sidebar/FooterItem";
 import MenuItem from "@/components/sidebar/MenuItem";
 import { motion, AnimatePresence } from "framer-motion";
