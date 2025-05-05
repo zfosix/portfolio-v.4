2e696d85ea689d4856fba8dad832801bdeadd5a5
zfosix <fajarfauzian53@gmail.com>
2025-05-06 06:04:13 +0700

    updated

--- src/types/projects.ts
+++ src/types/projects.ts
@@ -1,26 +1,26 @@
 export interface Project {
-    id: number;
-    title: string;
-    description: string;
-    technologies: string[];
-    image: string;
-    link: string;
-  }
-  
-  export interface Certificate {
-    id: number;
-    title: string;
-    description: string;
-    issuedBy: string;
-    date: string;
-    image: string;
-    link: string;
-  }
-  
-  export function isProject(item: Project | Certificate): item is Project {
-    return (item as Project).technologies !== undefined;
-  }
-  
-  export function isCertificate(item: Project | Certificate): item is Certificate {
-    return (item as Certificate).issuedBy !== undefined;
-  }
+  id: number;
+  title: string;
+  description: string;
+  technologies: string[];
+  image: string;
+  link: string;
+}
+
+export interface Certificate {
+  id: number;
+  title: string;
+  description: string;
+  issuedBy: string;
+  date: string;
+  image: string;
+  link: string;
+}
+
+export function isProject(item: Project | Certificate): item is Project {
+  return (item as Project).technologies !== undefined;
+}
+
+export function isCertificate(item: Project | Certificate): item is Certificate {
+  return (item as Certificate).issuedBy !== undefined;
+}
